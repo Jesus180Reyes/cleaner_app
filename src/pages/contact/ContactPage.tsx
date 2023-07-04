201bc5da06062fa3e9f9b309bfa72dd8ee10ce06
Edwin Corrales <www.edwinantonio40@gmail.com>
2023-07-03 23:06:22 -0600

    traduccion al ingles

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -5,11 +5,11 @@ export const ContactPage = () => {
     <>
     <Navbar/>
     <Banner 
-    title={"Contactanos"}
-    subtitle={"Podras comunicarte con nosotros y un asesor se pondra en contacto contigo"}
-    btnTitle={"Contactanos"} 
+    title={"Contact Us."}
+    subtitle={"You can contact us and an advisor will contact you!."}
+    
     imgUrl={"https://www.pinturapro.cl/wp-content/uploads/2017/09/pintura-casas.jpg"}/>
-    <h1 style={{textAlign: "center"}}>Contactanos</h1>
+    <h1 style={{textAlign: "center"}}>Contact Us.</h1>
     <div className="contact-container">
       <div className="map-container">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.611166037451!2d-87.18006402577284!3d13.31221260732087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f703ce471cc7225%3A0x4f642346d3a54ad6!2sUnimall!5e0!3m2!1ses-419!2shn!4v1688418137410!5m2!1ses-419!2shn" width="600" height="450"   loading="lazy"></iframe>
@@ -17,12 +17,12 @@ export const ContactPage = () => {
       <div className="form-container">
         <h2>Drop up a message</h2>
         <form >
-          <input type="text" placeholder="Tu nombre completo" />
-          <input type="email" placeholder="Tu Correo electronico" />
-          <input type="number" placeholder="Tu Numero de telefono" />
+          <input type="text" placeholder="Your full name:" />
+          <input type="email" placeholder="Your e-mail:" />
+          <input type="number" placeholder="Your phone number:" />
           <p>Fecha de reservacion</p>
-          <input type="date"  placeholder="Fecha de reservacion"/>
-          <button className="btn btn-cotizar">Cotizar</button>
+          <input type="date"  placeholder="reservation date:"/>
+          <button className="btn btn-cotizar">quote</button>
         </form>
       </div>
     </div>
--- src/pages/location/LocationPage.tsx
+++ src/pages/location/LocationPage.tsx
@@ -5,7 +5,7 @@ export const LocationPage = () => {
     <>
     <Navbar/>
     <Banner 
-    title={"Nuestra Ubicacion"}
+    title={"Our location"}
     subtitle={"ubicacion del sitio"}
     btnTitle={"Encuentranos"} 
     imgUrl={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKLpEPm2SQIkyyLZaa_AkY5XAhpBd61X754g&usqp=CAU"}/>
