a9c902e3d96bc0100a6e554325ba4bef3852ada6
Jesus Reyes MAC <luisdejesus200122@gmail.com>
2023-07-25 09:55:37 -0600

    Feat: Slider Added on LocationPage.tsx

--- src/pages/location/LocationPage.tsx
+++ src/pages/location/LocationPage.tsx
@@ -1,14 +1,44 @@
+import Slider from "react-slick"
 import { Banner, FloatingActionButton, Footer, Navbar } from "../../components/shared"
 
 export const LocationPage = () => {
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 5000,
+    autoplay:true,
+    slidesToShow: 1,
+    autoplaySpeed: 3500,
+    adaptiveHeight: true,
+    adaptiveWeight: true,
+    slidesToScroll: 1,
+    fade: true
+};
+
   return (
     <>
-    <Navbar/>
+   
+    <Slider {...settings} className="slider-carousel">
+   
     <Banner 
     title={"Our Gallery"}
     subtitle={""}
     btnTitle={""} 
-    imgUrl={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKLpEPm2SQIkyyLZaa_AkY5XAhpBd61X754g&usqp=CAU"}/>
+    imgUrl={"https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Anime_Girl.png/1280px-Anime_Girl.png"}/>
+    
+    <Banner 
+    title={"Our Gallery"}
+    subtitle={""}
+    btnTitle={""} 
+    imgUrl={"https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/09/16/16633380346741.jpg"}/>
+    <Banner 
+    title={"Our Gallery"}
+    subtitle={""}
+    btnTitle={""} 
+    imgUrl={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH8FJnVzdJy1N5TatPX8PzxfK6PCBJfoThtg&usqp=CAU"}/>
+    </Slider>
+    <Navbar/>
     <Footer/>
     <FloatingActionButton/>
 
